5eab87296b6897a2aac897c9d4f4405edc8d17f1
Contactsushil <contact.sushilpandey@gmail.com>
2024-04-13 11:45:45 +0530

    Built Profile & skelton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  data: any = {};
+  repodata: any = {};
+  loading: boolean = true;
+  repoloading: boolean = true;
+  error: string | null = null;
   searchInput: string = '';
 
-  handleInputChange() {
-    console.log(this.searchInput);
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    
+  }
+  loadrepo(): void {
+    this.apiService.getReposForUser(this.searchInput).then(
+      (response: any) => {
+        this.repodata = response;
+        console.log(this.repodata);
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+        this.error = 'Error fetching data. Please try again later.';
+        this.loading = false;
+      }
+    );
   }
 
-  handleSearch() {
-    console.log('Search button clicked!');
+  loadData(): void {
+    this.apiService.getUser(this.searchInput).then(
+      (response: any) => {
+        this.data = response;
+        console.log(this.data);
+        this.loading = false;
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+        this.error = 'Error fetching data. Please try again later.';
+        this.loading = false;
+      }
+    );
+  }
+
+  handleSearch(): void {
+    this.loadData();
+    this.loadrepo();
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -19,29 +19,18 @@ export class ApiService {
     });
   }
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
-  }
-
-  async getIssues(owner: string, repo: string) {
+  async getUser(username: string){
     try {
-      const result = await this.octokit.request("GET /repos/{owner}/{repo}/issues", {
-        owner: owner,
-        repo: repo,
-      });
-
-      const titleAndAuthor: IssueInfo[] = result.data.map(issue => ({
-        title: issue.title,
-        authorID: issue.user?.id ?? -1 
-      }));
-      console.log(titleAndAuthor);
-
-      return titleAndAuthor; 
-    } catch (error :any) {
-      console.log(`Error! Status: ${error.status}. Message: ${error.response.data.message}`);
-      throw error; 
+        const response = await this.octokit.request<any>(`GET /users/${username}`);
+        const userData = response.data;
+            return userData;
+    } catch (error:any) {
+        console.error(`Error fetching user data for ${username}: ${error.message}`);
+        throw error;
     }
-  }
+}
+
+
   async getReposForUser(username: string, page: number = 1, perPage: number = 10) {
     try {
       const response = await this.octokit.request<any>('GET /users/{username}/repos', {
@@ -50,7 +39,6 @@ export class ApiService {
         per_page: perPage
       });
       const data = response.data;
-      console.log(data);
 
       return data;
     } catch (error :any) {
